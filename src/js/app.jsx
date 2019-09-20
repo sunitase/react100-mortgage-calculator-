d30382eb5044c49ee6ef02aed9911320b940b6f9
Sunita <54287247+sunitase@users.noreply.github.com>
2019-09-20 12:20:17 -0700

    getting input for one variable in calculate function. Formula already in place

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -4,55 +4,63 @@ export default class App extends React.Component {
   // your Javascript goes here
   constructor(props){
     super(props)
-    this.state = {balance: 0, rate: 0, term: 15,}
+    this.state = {balance: '', rate: '', term: 15}
     this.handleChange = this.handleChange.bind(this);
-    this.handleCilck = this.handleClick.bind(this);
+    //this.onChange = this.onChange.bind(this);
+    //this.handleCilck = this.handleClick.bind(this);
+    this.calculate = this.calculate.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({balance: event.target.balance, rate: event.target.rate, term: event.target.term});
-}
-
-handleClick(e){
-  this.setState({output: e.target.M})
-}
+ 
 
 calculate(){
-  var M, P;
-  var balance; 
-  var term;
+console.log("test")
+  //var monthlyPay, P;
+const balance = this.state.balance;
+  //balance = (state.balance).value;
+
+ // rate = (this.state.rate).value;
+ // term = (this.state.term).value; 
+  console.log(balance);
+ /*console.log(rate);
+  console.log(term);
   var monthrate;
   monthrate= rate/12;
   var n; 
   //n= 12*15 for 15 yrs mortgage or 12*30 for 30 yrs mortgage
   P= balance;
+  if(term=15){
+    n=12*15;
+} 
+  else if(term=30){
+    n=12.30;
+    //var simple = Math.pow((1+monthrate), n)
+  } 
   var simple = Math.pow((1+monthrate), n)
   var numerator, denominator;
   numerator= mrate*simple;
   denominator= simple-1;
   var bracketvalue = numerator/denominator;
-  M = bracketvalue*P;
-  var output = M.toString();
+  monthlyPay = bracketvalue*P;
+  //var output = M.toString();
+ // return(monthlyPay)
+  //this.setState(M: output)
+  */
   
-
-
-  if(term=15){
-    n=12*15;
-    var simple = Math.pow((1+monthrate), n)
-
-
-  }
-  else if(term=30){
-    n=12.30;
-    var simple = Math.pow((1+monthrate), n)
-
-
-  } 
 }
 
+handleChange(event) {
+  //this.setState({balance: event.target.balance, rate: event.target.rate, term: event.target.term});
+  this.setState({[event.target.name]: event.target.value})
+}
+
+/*handleClick(e){
+ this.setState({output: e.target.monthlyPay})
+}*/
 
   render() {
     return (
+      <form>
       
       <div className='container'>
 
@@ -60,32 +68,32 @@ calculate(){
         <h3>Mortgage Calculator</h3>
         <div>
         <label>Loan Balance </label>
-        <input type="number"  name="balance" value={this.state.balance} onChange={this.handleChange.bind(this,'balance')}></input>
+        <input type= 'number'  name= 'balance' value={this.state.balance} onChange={this.handleChange}></input>
         </div>
 
         <div>
         <label>Interest Rate (%)</label>
-        <input type="number" name="rate" step="0.01" value={this.state.rate} onChange={this.handleChange.bind(this,'rate')}></input>
+        <input type="number" name="rate" step="0.01" value={this.state.rate} onChange={this.handleChange}></input>
         </div>
         <div>
         <label>Loan Term (yrs) </label>
-            <select name="term" value={this.state.term} onChange={this.handleChange.bind(this,'term')}>
+            <select name="term" value={this.state.term} onChange={this.handleChange}>
                 <option value="15">15</option>
                 <option value="30">30</option>        
             </select>
         </div>
         <div>
-        <button name="submit" onClick={this.calculate()}>Submit</button>
+        <button name="submit" onClick={this.calculate}>Submit</button>
         </div>
       
-         <div className="col-sm-12" name="output" id="output">Output</div>
-            
+         <div className="col-sm-12" name="output" id="output">${this.state.monthlyPay} is your monthly payment</div>
+
          </div>
   
-        {
-          
-        }
+      
       </div>
+    
+    </form>
     );
   }
 }
