34e293d3bf5cead63599aa8ab41b82aa4d510e69
Sunita <54287247+sunitase@users.noreply.github.com>
2019-09-20 17:48:05 -0700

    works check on formula again make it pretty

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -4,60 +4,76 @@ export default class App extends React.Component {
   // your Javascript goes here
   constructor(props){
     super(props)
-    this.state = {balance: 0, rate: 0, term: 15}
+    this.state = {balance: 0, rate: 0, term: 15, output: ''}
+    
     this.handleChange = this.handleChange.bind(this);
-    //this.onChange = this.onChange.bind(this);
-    //this.handleCilck = this.handleClick.bind(this);
+    this.handleClick = this.handleClick.bind(this);
     this.calculate = this.calculate.bind(this);
   }
 
  
 
-calculate(){
+calculate(event, balance, rate, term){
+  event.preventDefault()
 console.log("test")
-  //var monthlyPay, P;
-const balance = this.state.balance;
-
-
-
- // rate = (this.state.rate).value;
- // term = (this.state.term).value; 
+ balance = this.state.balance;
+ rate = this.state.rate;
+ term = this.state.term;
+ 
   console.log(balance);
- /*console.log(rate);
+  console.log(rate);
   console.log(term);
+  var monthlyPay;
   var monthrate;
-  monthrate= rate/12;
-  var n; 
-  //n= 12*15 for 15 yrs mortgage or 12*30 for 30 yrs mortgage
-  P= balance;
-  if(term=15){
-    n=12*15;
-} 
-  else if(term=30){
-    n=12.30;
-    //var simple = Math.pow((1+monthrate), n)
-  } 
-  var simple = Math.pow((1+monthrate), n)
+  var n;
+  /*
+  if (term == 15){
+    n = 15*12;
+    term=15;
+  }
+  else if (term==30)
+  {
+    n = 30*12;
+    term=30;
+  }
+ */
+  monthrate= rate/1200;
+  monthlyPay = balance * monthrate / (1 - (Math.pow(1/(1 + monthrate), term)));
+ var finalmonthly= monthlyPay/10;
+  console.log(monthrate);
+  console.log(n);
+  /*var getpowof=1+monthrate;
+  
+  var simplepowval = Math.pow(getpowof, n)
+  console.log(simplepowval);
   var numerator, denominator;
-  numerator= mrate*simple;
-  denominator= simple-1;
+  numerator= monthrate*simplepowval;
+  denominator= simplepowval-1;
   var bracketvalue = numerator/denominator;
-  monthlyPay = bracketvalue*P;
-  //var output = M.toString();
- // return(monthlyPay)
-  //this.setState(M: output)
-  */
+  monthlyPay = bracketvalue*balance;*/
+  var outp = finalmonthly.toFixed(2).toString();
+console.log(outp);
+
+return(this.setState({output : `$${outp} is your monthly payment`} ))
+ 
+  
+  
+  
   
 }
 
 handleChange(event) {
+  console.log(event.target.name, event.target.value)
   //this.setState({balance: event.target.balance, rate: event.target.rate, term: event.target.term});
   this.setState({[event.target.name]: event.target.value})
+
 }
 
-/*handleClick(e){
- this.setState({output: e.target.monthlyPay})
-}*/
+handleClick(){
+ console.log('hello')
+
+ //this.setState({e:target.finalmonthly})
+}
 
   render() {
     return (
@@ -66,32 +82,34 @@ handleChange(event) {
       <div className='container'>
 
       <div className='row'>
-        <h3>Mortgage Calculator</h3>
+        <h3 size="12">Mortgage Calculator</h3>
+        <br></br>
         <div>
-        <label>Loan Balance </label>
+        <label>Loan Balance      </label>
         <input type= 'number'  name= 'balance' value={this.state.balance} onChange={this.handleChange}></input>
         </div>
-
+        <br></br>
         <div>
-        <label>Interest Rate (%)</label>
+        <label>Interest Rate (%)      </label>
         <input type="number" name="rate" step="0.01" value={this.state.rate} onChange={this.handleChange}></input>
         </div>
         <div>
-        <label>Loan Term (yrs) </label>
+          <br></br>
+        <label>Loan Term (yrs)     </label>
             <select name="term" value={this.state.term} onChange={this.handleChange}>
                 <option value="15">15</option>
                 <option value="30">30</option>        
             </select>
         </div>
+        <br></br>
         <div>
-        <button name="submit" onClick={this.calculate}>Submit</button>
-        </div>
-      
-         <div className="col-sm-12" name="output" id="output">${this.state.monthlyPay} is your monthly payment</div>
-
+          <div>
+          <button  value={this.state.output} name="submit" onClick={this.calculate}>Submit</button>
+           <div name="output" id="output">{this.state.output}</div>
+          </div>
          </div>
   
-      
+      </div>
       </div>
     
     </form>
