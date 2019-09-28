d4e6f4dfdbf9e24bc5e41928f0b04be9934c6538
Sunita <54287247+sunitase@users.noreply.github.com>
2019-09-27 19:51:18 -0700

    some Bootstrap and need css

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -11,8 +11,6 @@ export default class App extends React.Component {
     this.calculate = this.calculate.bind(this);
   }
 
- 
-
 calculate(){
  const balance = this.state.balance;
  const rate = this.state.rate;
@@ -45,44 +43,69 @@ let calculate = this.calculate();
     return (
 
     
-      <form className="form-horizontal">
-      <div classNameName="container">
-      <h3>Mortgage Calculator</h3>
+      <form>
+       
+          <div className="topic pt-4 text-center bg-info shadow-sm p-4 mb-4">
+            <h3>Mortgage Calculator</h3>
+          </div>
+        
       <br></br>
-      <div className="row">
-      <div className="col-sm-3">
-          <label HTMLfor="loan">Loan Balance </label>
-      </div>
-      <div className="col-sm-9">
-          <input type= 'number'  name= 'balance' value={this.state.balance} onChange={this.handleChange}></input>
-          <br></br>
-      </div>
-      </div>
-      <div className="row">
-          <div className="col-sm-3">
-          <label HTMLfor="loan">Interest Rate (%)</label>
+   <div className="container">
+      
+        <div className="p-2 bg-info">
+            <div className="row">
+              <div className="col-sm-4">
+                <label HTMLfor="loan">Loan Balance </label>
+              </div>
+              <div className="col-sm-8">
+                <input type= 'number'  name= 'balance' value={this.state.balance} onChange={this.handleChange}></input>
+                <br></br>
+              </div>
           </div>
-          <div className="col-sm-9">
-          <input type="number" name="rate" step="0.01" value={this.state.rate} onChange={this.handleChange}></input>
-          
+      </div>
+     
+      <div className="p-2 bg-warning flex-fill">
+        <div className="row">
+              <div className="col-sm-4">
+              <label HTMLfor="loan">Interest Rate (%)</label>
+              </div>
+              <div className="col-sm-8">
+              <input type="number" name="rate" step="0.01" value={this.state.rate} onChange={this.handleChange}></input>
+              </div>
           </div>
+        </div>  
   
-          <div className="col-sm-3">
-          <label for="term">Loan Term (yrs) </label>
+        <div className="p-2 bg-info">
+      <div className="row">
+          <div className="col-sm-4">
+             <label for="term">Loan Term (yrs) </label>
            </div>
-          <div className="col-sm-9">
-          <select name="term" value={this.state.term} onChange={this.handleChange}>
+          <div className="col-sm-8">
+              <select name="term" value={this.state.term} onChange={this.handleChange}>
               <option value="15">15 yr</option>
               <option value="30">30 yr</option>        
-          </select>
+              </select>
           </div>
           <br></br>
-          <div className="col-sm-12">
-          <button value={this.state.output} name="submit" onClick={this.handleClick}>Calculate</button>
-          <div className="col-sm-12" name="output" id="output">{this.state.output}</div>
           </div>
+          </div>
+        <div>
+          <br></br>
+        </div>
+        <div className="d-flex justify-content-center">
+        <div className="row">
+          <div className="col">      
+          <div className="d-flex justify-content-center">
+                <button value={this.state.output} name="submit" onClick={this.handleClick}>Calculate</button>
+                <div className="col" name="output" id="output">{this.state.output}</div>
+          </div>
+          </div>
+        </div>
+        </div>
+
+
       </div>
-      </div>
+    
   </form>
   );
   }
