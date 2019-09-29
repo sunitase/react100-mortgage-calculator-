5c4105480e3e59e89425ff1c8dfc9b5cdc9578d1
Sunita <54287247+sunitase@users.noreply.github.com>
2019-09-29 14:35:16 -0700

    ready to submit passing tests- does not like divs and spaces used to timeout and give error on input element name

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -41,66 +41,41 @@ let calculate = this.calculate();
 
   render() {
     return (
-      <form>
-       
-          <div className="topic pt-4 text-center bg-info shadow-sm p-4 mb-4">
-            <h3>Mortgage Calculator</h3>
-          </div>
+  <form>
+    <div className="topic pt-4 text-center bg-info shadow-sm p-4 mb-4">
+        <h3>Mortgage Calculator</h3>
+    </div>
         
-      <br></br>
    <div className="container">
-      
-        <div className="p-2 bg-info">
-            <div className="row">
-              <div className="col-sm-4">
-                <label HTMLfor="loan">Loan Balance </label>
-              </div>
 
-              <div className="col-sm-8">
-                <input name="balance" id="balance" type="number" value={this.state.balance} onChange={this.handleChange} />
-              </div>
-          </div>
-      </div>
+            <div className="p-2 bg-info">
+                  
+                  <div className="row">
+                            <label HTMLfor="loan">Loan Balance </label>
+                            <input name="balance" id="balance" type="number" value={this.state.balance} onChange={this.handleChange} />       
+                  </div>      
+            </div>
      
-      <div className="p-2 bg-warning flex-fill">
-        <div className="row">
-              <div className="col-sm-4">
-              <label htmlFor="loan">Interest Rate (%)</label>
-              </div>
-              <div className="col-sm-8">
-              <input type="number" name="rate" step="0.01" value={this.state.rate} onChange={this.handleChange}></input>
-              </div>
-          </div>
-        </div>  
-  
-        <div className="p-2 bg-info">
-      <div className="row">
-          <div className="col-sm-4">
-             <label htmlFor="term">Loan Term (yrs) </label>
-           </div>
-          <div className="col-sm-8">
-              <select name="term" id="term" value={this.state.term} onChange={this.handleChange}>
-              <option value="15">15 yr</option>
-              <option value="30">30 yr</option>        
-              </select>
-          </div>
-          <br></br>
-          </div>
-          </div>
-        <div>
-          <br></br>
-        </div>
-        <div className="d-flex justify-content-center">
-        <div className="row">
-          <div className="col">      
-          <div className="d-flex justify-content-center">
-                <button value={this.state.output} name="submit" onClick={this.handleClick}>Calculate</button>
-                <div className="col" name="output" id="output">{this.state.output}</div>
-          </div>
-          </div>
-        </div>
-        </div>
-
+            <div className="p-2 bg-warning flex-fill">
+                  <div className="row"> 
+                        <label htmlFor="rate">Interest Rate (%)</label>
+                        <input type="number" name="rate" step="0.01" value={this.state.rate} onChange={this.handleChange}></input>   
+                  </div>
+              </div>  
+              
+               <div className="row">
+                        <label>Loan Term (yrs) </label>
+                           <select name="term" value={this.state.term} onChange={this.handleChange}>
+                            <option value="15">15 yr</option>
+                            <option value="30">30 yr</option>        
+                          </select>
+                </div>
+            
+                <div>
+                  <button value={this.state.output} name="submit" onClick={this.handleClick}>Calculate</button>
+                  <div className="col" name="output" id="output">{this.state.output}</div>       
+                </div>
+                  
       </div>   
   </form>
   );
